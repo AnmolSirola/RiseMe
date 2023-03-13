c998e86067e08195df09dcbefc7ea67d23b04cb7
Anmol Sirola <anmolsirola57@gmail.com>
2023-03-13 22:15:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Home from "./components/Home"
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Home />
+    </div>
+  )
+}
+
+export default App
